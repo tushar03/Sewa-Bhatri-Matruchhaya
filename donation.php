dca7ba62f2a50da600ad8bfd1c50eccb40a2da98
tushar03 <patanker.tushar@yahoo.com>
2015-07-22 21:38:21 +0530

    Create donation.php

---
+++ donation.php
@@ -0,0 +1,127 @@
+<html>
+<title>Donations</title>
+<style type="text/css">
+
+body {
+	background-repeat: no-repeat;
+	}
+</style>
+<script type="text/javascript">
+
+function TableHeight() 
+{
+  if (typeof (window.innerWidth) == 'number') 
+  {
+    //Non-IE 
+    myWidth = window.innerWidth;
+    myHeight = window.innerHeight;
+  }
+  else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) 
+  {
+    //IE 6+ in 'standards compliant mode' 
+    myWidth = document.documentElement.clientWidth;
+    myHeight = document.documentElement.clientHeight;
+  }
+  else if (document.body && (document.body.clientWidth || document.body.clientHeight)) 
+  {
+    //IE 4 compatible 
+    myWidth = document.body.clientWidth;
+    myHeight = document.body.clientHeight;
+  }
+  document.getElementById('ctl00_TopPanel').style.height = (myHeight - 140) + 'px';
+}
+
+
+</script>
+<script type="text/javascript">
+
+function TableHeight() 
+{
+  if (typeof (window.innerWidth) == 'number') 
+  {
+    //Non-IE 
+    myWidth = window.innerWidth;
+    myHeight = window.innerHeight;
+  }
+  else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) 
+  {
+    //IE 6+ in 'standards compliant mode' 
+    myWidth = document.documentElement.clientWidth;
+    myHeight = document.documentElement.clientHeight;
+  }
+  else if (document.body && (document.body.clientWidth || document.body.clientHeight)) 
+  {
+    //IE 4 compatible 
+    myWidth = document.body.clientWidth;
+    myHeight = document.body.clientHeight;
+  }
+  document.getElementById('ctl00_TopPanel').style.height = (myHeight - 140) + 'px';
+}
+
+
+</script>
+<head>
+<link rel="shortcut icon" href="sevabharti.ico">
+</head>
+<body bgcolor="#40BCBC" oncontextmenu="return false">
+<h1><center>  
+          <font  size="10px"><u>Donations</u></font><img src="images/logo.jpg" width="167" height="167" align="left"><img src="images/donations.jpg" width="167" height="167" align="right"></font>
+</center></h1><br/><br/><br/><br/><br/><br/>
+<font size="5px">You can also help us in caring of the children.You can help us by donating the following items at any branch of Matruchhaya and the items are:-</font>
+<br/><br/>
+             <table align="center"> <font size="3px">
+				<tr>
+					<td><font size="3px">1. Lactogen No.1 milk powder</font></td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>        <td><font size="3px">2. Dettol </font></td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td><font size="3px">3. Coconut/Mustard oil </font></td>
+				</tr>	&nbsp;
+                 <tr>
+					<td><font size="3px">4. Rice/Rahar Dal</font></td>   </td><td><font size="3px">5. phenyle </font></td><td><font size="3px">6. Johnson soap/powder</font> </td>
+				</tr>&nbsp;
+				<tr>
+					<td><font size="3px">7. Edible Oil</font></td><td><font size="3px">8. Wheat Flour</font></td><td><font size="3px">9. Sugar</font></td><td>
+				</tr>&nbsp;
+				<tr>
+					<td><font size="3px">10. Tea</font></td><td><font size="3px">11.Washing Powder</font></td>
+				</tr>&nbsp;
+				
+				<tr>
+					<td><font size="3px">12. Woollen and cotton Clothes for children</font></td><td><font size="3px">13. Huggies</font></td>
+				</tr></font>
+			</table>
+
+
+
+&nbsp;&nbsp; <br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size="10"><img src="images/donationbox.jpg" width="194" height="190" align="right"></font>
+<h2 align="center">	 OR</h2>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+<div align="center"><font size="5px">You can donate us through cash/Demand Draft/Cheque. </font></div>
+<div align="center"><font size="5px">To donate us through bank account  &nbsp;&nbsp;</font><a href="bankdonation.html"><font size="6px" color="red">Click Here</font></a> 
+
+<br/>
+<br/>
+<br/>
+<marquee scrollamount="5"><font size="5px" color="white">Online Donation Facility is NOT Available at the Moment!!!! Sorry For The Inconvenience.</font></marquee>
+<p>
+      <h1><marquee scrollamount="8"><font color="red" size="6px">NOTE:-</font> <font>These donations are free from income tax u/s 80G.</font></marquee></h1> 
+</p>
+
+</body>
+<SCRIPT language=JavaScript>
+	var message = "function disabled"; 
+	function rtclickcheck(keyp)
+	{ 
+		if (navigator.appName == "Netscape" && keyp.which == 30)
+		{ 
+			alert(message);
+			return false;
+		} 
+		if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 20) 
+		{ 	
+			alert(message); 	
+			return false;
+		} 
+	} 
+	document.onmousedown = rtclickcheck;
+</SCRIPT>
+</html>
